79a41a4afb864a1f2edf20a0d05a7761f03196ae
Or <orbenda1905@gmail.com>
2016-01-23 15:01:36 +0200

    database connection

---
+++ mobile/includes/db.php
@@ -0,0 +1,12 @@
+<?php
+$dbhost = "166.62.8.11";
+$dbuser = "auxstudDB5";
+$dbpass = "auxstud5DB1!";
+$dbname = "auxstudDB5";
+
+$connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+if(mysqli_connect_errno()) {
+	die("DB connection failed: " . mysqli_connect_error() . " (" . mysqli_connect_errno() . ")" );
+}
+?>
