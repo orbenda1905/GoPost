9cf92259857c9fdeec83030ceff00b2e42a11497
Or <orbenda1905@gmail.com>
2016-02-07 10:29:47 +0200

    v1.3 - adding delivery boxes script

--- mobile/db.php
+++ mobile/db.php
@@ -9,4 +9,6 @@ $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 if(mysqli_connect_errno()) {
 	die("DB connection failed: " . mysqli_connect_error() . " (" . mysqli_connect_errno() . ")" );
 }
+
+mysqli_set_charset($connection, 'utf8'); //allowing the databse to display Hebrew
 ?>
--- mobile/includes/script.js
+++ mobile/includes/script.js
@@ -1,3 +1,18 @@
+var Client = (function (pName, pId) {//class represents client
+	var name = pName;
+	var id = pId;
+
+	this.getName = function () {
+		return name;
+	};
+
+	this.getId = function () {
+		return id;
+	};
+})
+
+var Shani = new Client("שני", 123456789);
+
 /*-----floating menu script-----*/
 $(function($){
         $('#menu').click(function(){
@@ -42,4 +57,45 @@ function message() {
     document.getElementById('light').style.display='none';
     document.getElementById('fade').style.display='none';
     });
+}
+
+function addBox(data) {
+	var box = $("<section class='deliveryBox'></section>");
+	var numberOfBoxes = $(".deliveryBox").length;
+	box.append("<div class='rec'><p>" + (numberOfBoxes+1) + "</p></div>");
+	var infoSection = $("<section class='info'></section>");
+	infoSection.append("<p><b>שולח: </b>" + data.from + "</p>" +
+								"<p><b>סטטוס: </b>" + data.status + "</p>");
+	box.append(infoSection);
+	var statusClass = $("<div class='status'></div>");
+	if (data.status === "מוכן") {
+		statusClass.append("<a class='readySts' href=pickupOptions.html?boxId=" + data.id + "></a>");
+	}else {
+		var infoBtn = $("<a class='infoSts' href='#'></a>");
+		infoBtn.on("click", function() {
+//			displayInfo(data.id);
+			alert(data.id);
+		});
+		statusClass.append(infoBtn);
+	}
+	box.append(statusClass);
+	box.append("<div class='clear'></div>");
+	$("main").append(box);
+}
+
+function deliveryList() {
+	$("#deliveryAdding").submit(function(e) {
+		e.preventDefault();
+		var boxId = $(".packageId").val();
+		$.get("addBox.php", {pId: Shani.getId(), boxId: boxId}, function(data) {
+			var json = $.parseJSON(data);
+			console.log(json);
+			if (json.exist === "no") {
+//				notExist(boxId);
+				alert("box number " + boxId + " not exist");
+				return;
+			}
+			addBox(json);
+		});
+	});
 }
