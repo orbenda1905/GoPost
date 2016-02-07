5f9c82d07759b3e26ffc7b1d3e9f7a07400c190c
yahav10 <tommyahav@gmail.com>
2016-02-07 19:15:04 +0200

    .

--- mobile/includes/script.js
+++ mobile/includes/script.js
@@ -12,17 +12,17 @@ var size = info.length;
     for (var i = 0; i < size; i++) {
         info[i].addEventListener("click", function() {
             window.scrollTo(0,0);
-            document.getElementById('light').style.display='block';
+ document.getElementById('light').style.display='block';
             document.getElementById('fade').style.display='block';
         });
     }
-    document.getElementById('fade').addEventListener("click", function() {
+ document.getElementById('fade').addEventListener("click", function() {
     document.getElementById('light').style.display='none';
     document.getElementById('fade').style.display='none';
     });
 }
 
-window.document.onkeydown = function (e){
+    window.document.onkeydown = function (e){
     if (!e){
         e = event;
     }
@@ -43,3 +43,4 @@ var send = document.getElementsByClassName('sendButton')[0];
     document.getElementById('fade').style.display='none';
     });
 }
+
