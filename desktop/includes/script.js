69eef1dc4fc11530df7e2e911bc4702cd770a3ee
Or <orbenda1905@gmail.com>
2016-02-10 13:39:58 +0200

    1.8

---
+++ desktop/includes/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	showCurrentDate();
+
+})
+
+function showCurrentDate() {
+	var date = new Date();
+	var day = date.getDate();
+	var month = date.getMonth() + 1;
+	var year = date.getFullYear();
+	$(".date").html("<p>" + day + "/" + month + "/" + year + "</p>");
+}
